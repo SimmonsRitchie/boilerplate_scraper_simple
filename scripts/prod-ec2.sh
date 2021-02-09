132237425f4426325581ed0cbfbac660de9892bf
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-02-10 07:21:18 +1300

    Provide better script boilerplate

--- scripts/prod-ec2.sh
+++ scripts/prod-ec2.sh
@@ -4,13 +4,14 @@
 # at regular intervals.
 
 # Start
+PROJECT_NAME="new_scraper"
 echo "##############################"
 echo $(date '+%Y-%m-%d %H:%M:%S')
 echo "PROD EC2 script start"
 # This tells pipenv to use this .env file
-export PIPENV_DOTENV_LOCATION="/home/dansr/projects/scraper_pa_vax_providers/.env"
+export PIPENV_DOTENV_LOCATION="/home/dansr/projects/${PROJECT_NAME}/.env"
 # Navigate to scraper project directory
-cd /home/dansr/projects/scraper_pa_vax_providers/
+cd "/home/dansr/projects/${PROJECT_NAME}/"
 # Run program
 # Note: In this case executing path to binary due to problems with pyenv working in cron
 ~/.pyenv/versions/3.6.10/bin/pipenv run python src/run.py
