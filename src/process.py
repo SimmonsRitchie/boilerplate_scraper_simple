d84ca25580585b2bbc956756ade330c905baf450
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-02-10 07:26:16 +1300

    Clean up and CSV compare func

--- src/process.py
+++ src/process.py
@@ -1,34 +1,49 @@
 import logging
 import json
-from typing import Dict
+from typing import Dict, List, Any
 
 import pandas as pd
 import pytz
 
-from definitions import PATH_FETCHED_METADATA, PATH_FETCHED_DATA_JSON
+from definitions import PATH_FETCHED_METADATA, PATH_FETCHED_DATA_JSON, PATH_FETCHED_DATA_CSV
 from datetime import datetime
 
 from helper.time import convert_utc_to_eastern
 
 
-def has_json_changed(json_new) -> bool:
+def has_json_changed(json_new: List[Dict[str, Any]]) -> bool:
     """
     Check if data has changed.
 
     Args:
-        json_new (pd.Dataframe): Fetched data as DataFrame
+        json_new (List[Dict[str, Any]]): Fetched data
 
     Returns:
         bool. True if data has changed.
 
     """
-
-    # No prior data, immediately return False
-
     # Return True if new data doesn't match old data
     with open(PATH_FETCHED_DATA_JSON, "r") as fin:
         json_old = json.load(fin)
     return not json_new == json_old
+
+
+def has_csv_changed(df_new: pd.DataFrame) -> bool:
+    """
+    Check if CSV data, provided as a Pandas DataFrame, has the same shape and values
+     as another CSV loaded as a Pandas Dataframe.
+
+    Args:
+        df_new (pd.Dataframe): CSV in DataFrame fprm
+
+    Returns:
+        bool. True if data is different than .
+
+    """
+
+    df_old = pd.read_csv(PATH_FETCHED_DATA_CSV)
+    matches = df_old.equals(df_new)
+    return not matches
 
 
 def create_metadata(scrape_time_utc: datetime, df: pd.DataFrame) -> Dict:
